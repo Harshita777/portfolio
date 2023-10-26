1ccb2d644ab2b27cb711dabeed655090e98ddc3d
harshitakumari4623 <harshitakumari4623@gmail.com>
2023-10-26 21:09:00 +0530

    completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="bg-emerald-100 ">
       <div className="mx-auto max-w-7xl px-8 py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8">
         <h2 className="text-3xl font-bold">
-          Web<span className="text-emerald-600">Dev</span>
+          Port<span className="text-emerald-600">folio</span>
         </h2>
         <div className="flex gap-x-3">
           {links.map((link) => {
